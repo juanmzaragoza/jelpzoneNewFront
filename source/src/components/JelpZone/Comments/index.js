707b42f7a0521c0955b05131e0b9a87093c1d168
Diego Gutierrez <diego.gutierrez@evolutions-it.net>
2018-07-17 23:29:35 -0300

    Cambio algunos aspectos de comentarios

--- source/src/components/JelpZone/Comments/index.js
+++ source/src/components/JelpZone/Comments/index.js
@@ -16,13 +16,13 @@ class Comments extends Component {
     return (
       <div>
         <h3 className="card-footer">
-          <i className="zmdi zmdi-comment-text mr-2"></i><IntlMessages id="appModule.Comments.CommentsTitle"/>
+          <i className="zmdi zmdi-comment-text mr-1"></i><IntlMessages id="appModule.Comments.CommentsTitle"/>
         </h3>
-        <div className="card-body">
+        <ul className="list list-unstyled mr-3 ml-3 mt-3 mb-3">
         {this.props.comments.map(
           (comment, index) => <Comment key={index} commentData={comment}/>
         )}
-        </div>
+        </ul>
       </div>
     );
   }
@@ -40,36 +40,34 @@ class Comment extends Component {
 
   render(){
     return(
-      <div className="media user-profile" key={this.props.commentData.id}>
-
+      <li className="media user-profile media-list mb-1 pb-1" key={this.props.commentData.id}>
           <Avatar
               alt={this.props.commentData.userName}
               src={this.props.commentData.image}
               className="user-avatar mr-2"
           />
-
         <div className="media-body align-self-center">
-          <h5 className="mb-1">{this.props.commentData.userName} @ {this.props.commentData.date}:</h5>
-          {this.props.commentData.description}
-          <hr/>
+          <h5 className="mb-1">{this.props.commentData.userName} <span className="meta-date text-right">{this.props.commentData.date}</span></h5>
+          <div className="meta-comment mb-1">{this.props.commentData.description}</div>
+
           <div className="btn-group-mins text-right">
             <Tooltip id="tooltip-like" title={<IntlMessages id="appModule.likes.like"/>}>
             <a
-              className="btn btn-xs bg-primary"
+              className="btn jr-btn-xs bg-primary"
             >
               <i className="zmdi zmdi-thumb-up text-white mr-1"></i>
             </a>
             </Tooltip>
             <Tooltip id="tooltip-dislike" title={<IntlMessages id="appModule.likes.disLike"/>}>
             <a
-              className="btn btn-xs bg-info"
+              className="btn jr-btn-xs bg-info"
             >
-              <i className="zmdi zmdi-thumb-down text-white mr-1"></i>
+              <i className="zmdi zmdi-thumb-down text-white"></i>
             </a>
             </Tooltip>
           </div>
         </div>
-      </div>
+      </li>
     );
   }
 
